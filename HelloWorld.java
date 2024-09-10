fcfced6f22aee56d60f2c646f320bf7a4f6f2637
Jonathan <jmende25@lab.cpcc.edu>
2024-09-10 17:02:45 -0400

    Statement Added To Hello World
    Added another print statement to the program

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello, World");
+        
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
     }
 } 
